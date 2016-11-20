7ea069d925c311f269ee627515c619e66b741ff5
Krid Jinklub <jinklub.krid@bit.edu.cn>
2016-11-20 20:37:55 +0800

    Imgrated from personal repo
    Imgrated from https://github.com/vasopikof/Bluemix-CCPX-blockchain, and
    set bluemix's Continuos Delivery to monitor this instead of old one.

---
+++ routes/site_router.js
@@ -0,0 +1,77 @@
+'use strict';
+/* global process */
+/*******************************************************************************
+ * Copyright (c) 2015 IBM Corp.
+ *
+ * All rights reserved. 
+ *
+ * Contributors:
+ *   David Huffman - Initial implementation
+ *******************************************************************************/
+var express = require('express');
+var router = express.Router();
+var setup = require('../setup.js');
+
+//anything in here gets passed to JADE template engine
+function build_bag(){
+	return {
+				setup: setup,								//static vars for configuration settings
+				e: process.error,							//send any setup errors
+				jshash: process.env.cachebust_js,			//js cache busting hash (not important)
+				csshash: process.env.cachebust_css,			//css cache busting hash (not important)
+			};
+}
+
+// ============================================================================================================================
+// Home
+// ============================================================================================================================
+router.route('/').get(function(req, res){
+	res.json({ "message": 'CCPX-webservices' }); 
+});
+
+// ============================================================================================================================
+// Part 1
+// ============================================================================================================================
+router.route('/p1').get(function(req, res){
+	res.render('part1', {title: 'Marbles Part 1', bag: build_bag()});
+});
+router.route('/p1/:page?').get(function(req, res){
+	res.render('part1', {title: 'Marbles Part 1', bag: build_bag()});
+});
+
+// ============================================================================================================================
+// Part 2
+// ============================================================================================================================
+router.route('/p2').get(function(req, res){
+	res.render('part2', {title: 'Marbles Part 2', bag: build_bag()});
+});
+router.route('/p2/:page?').get(function(req, res){
+	res.render('part2', {title: 'Marbles Part 2', bag: build_bag()});
+});
+
+// ============================================================================================================================
+// CCPX-blockchain webservices
+// ============================================================================================================================
+router.post('/',function(req, res,next){
+	res.json({ "message": 'Service got post requested' }); 
+});
+/*router.post('/storeTx',(function(req, res){
+	res.json({ 
+		"respond": 100,
+		"content": req.param('arg') 
+	}); 
+});
+router.post('/showTx',function(req, res){
+	res.json({ 
+		"respond": 300,
+		"content": {
+			"seller_ID":req.param('arg'),
+			"TX":[
+				{"foo":"bar"},
+				{"foo":"bar"}
+			]
+		}
+	}); 
+});*/
+
+module.exports = router;
