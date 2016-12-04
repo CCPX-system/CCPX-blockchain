855480f1f8ef9a552e31b51537a84be6b17a4177
Krid Jinklub <jinklub.krid@bit.edu.cn>
2016-12-04 08:10:05 +0800

    Un auto deploy and add test date

--- docker-webservice/server.js
+++ docker-webservice/server.js
@@ -48,7 +48,7 @@
         //app2.setup(ibc, cc);
 
     // Step 4 ==================================
-        if(true){                //decide if I need to deploy or not
+        if(false){                //decide if I need to deploy or not
             g_cc = cc;
             cc.deploy('init', ['99'], {delay_ms: 30000}, function(e){                       //delay_ms is milliseconds to wait after deploy for conatiner to start, 50sec recommended
                 console.log("success deployed");
@@ -181,8 +181,12 @@
 //-----------------API FOR DEV--------------------------------------------------------
 
 
-    app.get('/testGet', function(req, res){
-      res.json({"msg":"test"});
+    app.post('/testPostDate', function(req, res){
+        var dd = req.body.day;
+        res.json({
+            "server":new Date().toString(),
+            "client":dd
+        });
     });
     app.get('/query_point', function(req, res){
         console.log('got read request');
