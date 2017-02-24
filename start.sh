bb88f2f32b5a9ffcee2368cc38c6b45915f5f752
Krid Jin <jinklub.krid@bit.edu.cn>
2017-02-24 13:51:51 +0800

    Avoid tomcat default port

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#docker stop $(docker ps -a -q)
-#docker rm -f $(docker ps -a -q)
-#docker rmi -f $(docker images -q)
+docker stop $(docker ps -a -q)
+docker rm -f $(docker ps -a -q)
+docker rmi -f $(docker images -q)
 
 cd docker-hyperledger
 . setenv.sh
@@ -13,5 +13,5 @@ docker build -t ccpx/ws .
 
 docker network connect bridge dockerhyperledger_vp_1
 
-docker run --name ccpx_node --net=bridge -p 9999:8080 ccpx/ws
-#docker run --name ccpx_node --net=bridge -d -p 9999:8080 ccpx/ws
+#docker run --name ccpx_node --net=bridge -p 9999:8088 ccpx/ws
+docker run --name ccpx_node --net=bridge -d -p 9999:8088 ccpx/ws
