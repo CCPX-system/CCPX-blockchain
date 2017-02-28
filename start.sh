a85ada099708c417d2c4ab2964c58dd69965ad2b
Krid Jin <jinklub.krid@gmail.com>
2017-02-28 23:10:57 +0800

    use none-detach as default for demo purpose

--- start.sh
+++ start.sh
@@ -19,5 +19,5 @@ docker build -t ccpx/ws .
 
 docker network connect bridge dockerhyperledger_vp_1
 
-#docker run --name ccpx_node --net=bridge -p 9999:8088 ccpx/ws
-docker run --name ccpx_node --net=bridge -d -p 9999:8088 ccpx/ws
+docker run --name ccpx_node --net=bridge -p 9999:8088 ccpx/ws
+#docker run --name ccpx_node --net=bridge -d -p 9999:8088 ccpx/ws
